2adc4f24cb3454638bd2df639731ab1e29f86fa0
carloswulfrano <carlosdanielwulfranocruz@gmail.com>
2026-09-01 01:00:37 -0600

    Ultimos ajustes tarea 2

--- src/main/java/Tareas/Tarea2/Main_Tarea2.java
+++ src/main/java/Tareas/Tarea2/Main_Tarea2.java
@@ -1,9 +1,8 @@
 import Tareas.Tarea2.NominaICO;
 
 void main() {
-        // Año de referencia para calcular la antigüedad de cada empleado.
-        // Ajusta este valor si tu profesor pide que se calcule con otro año.
-        int anioActual = 2024;
+        //Año de referencia
+        int anioActual = 2026;
 
         NominaICO nomina = new NominaICO("junio.dat", anioActual);
 
